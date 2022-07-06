7a0c6be5eba06e956accb66f90022f5279090406
rdshoep <rdshoep@gmail.com>
2022-07-06 15:17:48 +0800

    feat: 优化分割方式，提高书写效率

--- src/utils/window-event.ts
+++ src/utils/window-event.ts
@@ -1,20 +1,26 @@
-window.onload = () => {
-    window.onresize = () => {
-        const windowWidth = document.body.clientWidth;
-        const el = document.querySelector('.rs-view-inner') as HTMLElement;
-        if (!el) {
-            return;
-        }
-        if (windowWidth < 1250 && windowWidth > 1000) {
-            const resetWidth = windowWidth - 450;
-            const marginWidth = resetWidth * 0.2;
-            const radio = Math.round(resetWidth * 0.8 / 794 * 100);
-            el.style.transform = `scale(${radio/100})`;
-            el.style.marginLeft = `${marginWidth / 2}px`;
-        } else if (windowWidth >= 1250) {
-            el.style.transform = `scale(1)`;
-            el.style.marginLeft = `auto`;
-        }
+export const handleWindowResize = () => {
+    const windowWidth = document.body.clientWidth;
+    const el = document.querySelector('.rs-view-inner') as HTMLElement;
+    if (!el) {
+        return;
     }
+    const $container = document.querySelector('.SplitPane') as HTMLElement;
+    const $leftPanel = $container.querySelector('.Pane.Pane1') as HTMLElement;
+    const $resizer = $container.querySelector('.Resizer') as HTMLElement;
+    const $leftPanel2 = $container.querySelector('.Pane.Pane2') as HTMLElement;
+    const resetWidth = windowWidth - $leftPanel.clientWidth - $resizer.clientWidth;
+    if (resetWidth >= 1000) {
+        el.style.transform = `scale(1)`;
+        el.style.marginLeft = `auto`;
+    } else {
+        $leftPanel2.style.width = `${resetWidth}px`;
+        const $pane2Wrapper = $leftPanel2.querySelector('.rs-view-wrapper') as HTMLElement;
+        const marginWidth = resetWidth * 0.2;
+        const radio = Math.round(resetWidth * 0.8 / 794 * 100);
+        el.style.transform = `scale(${radio / 100})`;
+        el.style.margin = `40px ${marginWidth / 2}px 40px ${marginWidth / 2}px`;
+    }
+};
+window.onload = () => {
+    window.onresize = handleWindowResize;
 }
-export { };
